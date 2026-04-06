289e2085a05b851fc8c189d8d534db8be0ead640
dzakwan24si <dzakwan24si@mahasiswa.pcr.ac.id>
2026-04-06 08:07:06 +0700

    Pengiriman Data Intent

--- app/src/main/java/com/example/dzakwan_apps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/dzakwan_apps/pertemuan_4/FourthActivity.kt
@@ -1,37 +1,41 @@
-package com.example.dzakwan_apps
+package com.example.dzakwan_apps.pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dzakwan_apps.MainActivity
+import com.example.dzakwan_apps.R
 import com.example.dzakwan_apps.databinding.ActivityFourthBinding
-import com.example.dzakwan_apps.databinding.ActivityMainBinding
 import com.example.dzakwan_apps.databinding.ActivityThirdBinding
 import com.example.dzakwan_apps.pertemuan_3.ThirdResultActivity
-import com.example.dzakwan_apps.pertemuan_4.FourthActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityFourthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setContentView(R.layout.activity_fourth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.btnToFourth.setOnClickListener {
-            val intent = Intent(this, FourthActivity::class.java)
-            intent.putExtra("nama", "Politeknik Caltex Riau")
-            intent.putExtra("asal", "Rumbai")
-            intent.putExtra("umur", 25)
+        binding.btnToMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        val nama = intent.getStringExtra("nama")
+        val asal = intent.getStringExtra("asal")
+        val umur = intent.getIntExtra("umur",0)
+        Log.i("== Data Intent ==","Nama: $nama , Usia: $umur, Asal: $asal")
     }
 }
